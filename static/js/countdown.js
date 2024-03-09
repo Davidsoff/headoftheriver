37845ef084edfe25ef81934129d18b4cbdb6b8c4
GSAL <glsvdsalm@gmail.com>
2024-03-09 12:45:05 +0100

    countdown off

--- static/js/countdown.js
+++ static/js/countdown.js
@@ -12,17 +12,17 @@
             distance = countDown - now;
 
         document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
         //do something later when date is reached
-        // if (distance < 0) {
-           //  document.getElementById("headline").innerText = "It's my birthday!";
-            // document.getElementById("countdown").style.display = "none";
-            // document.getElementById("content").style.display = "block";
-            // clearInterval(x);
-        // }
+        if (distance < 0) {
+            //document.getElementById("headline").innerText = "It's my birthday!";
+            document.getElementById("countdown").style.display = "none";
+            //document.getElementById("content").style.display = "block";
+            //clearInterval(x);
+         }
         //seconds 
     }, 0)
 }());
